9539dab7bef9e701c895f0828297f511f0fa5060
Ahmad Zahradeen Tahir <aztahir20@gmail.com>
2023-07-19 15:16:44 +0100

    Fixed responsiveness

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,35 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Contact.module.css";
-import {MdOutlineMail} from 'react-icons/md'
-import {BsWhatsapp} from 'react-icons/bs'
-import Lottie from 'lottie-react'
-import anim from '../../assets/84584-contact-us.json'
+import { MdOutlineMail } from "react-icons/md";
+import { BsWhatsapp } from "react-icons/bs";
+import Lottie from "lottie-react";
+import anim from "../../assets/84584-contact-us.json";
 
 const Contact = () => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredmail, setEnteredMail] = useState("");
+  const [enteredText, setEnteredText] = useState("");
+  const [showStatus, setShowStatus] = useState(false);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const data = {
+      name: enteredName,
+      email: enteredmail,
+      text: enteredText,
+    };
+    console.log(data);
+    setShowStatus(true);
+    setEnteredName("");
+    setEnteredMail("");
+    setEnteredText("");
+    setTimeout(() => {
+      setShowStatus(false);
+    }, 3000);
+  };
+
   return (
     <section className={classes.contact} id="contact">
       <h1>Contact Me</h1>
       <main>
         <div className={classes.left}>
           <div className={classes.comops}>
-            <div className={classes['comop-contain']}>
-            <MdOutlineMail />
+            <div className={classes["comop-contain"]}>
+              <MdOutlineMail />
               <h4>Email</h4>
               <h5>aztahir20@gmail.com</h5>
-              <a href="mailto:aztahir20@gmail.com" target='_blank'>Send a mail</a>
+              <a href="mailto:aztahir20@gmail.com" target="_blank">
+                Send a mail
+              </a>
             </div>
-            <div className={classes['comop-contain']}>
-            <BsWhatsapp />
+            <div className={classes["comop-contain"]}>
+              <BsWhatsapp />
               <h4>WhatsApp</h4>
               <h5>+2349023064514</h5>
-              <a href="https://wa.link/nqnzpn" target='_blank'>Send a message</a>
+              <a href="https://wa.link/nqnzpn" target="_blank">
+                Send a message
+              </a>
             </div>
           </div>
-          <form>
-            <input type="text" name="name" placeholder="Full Name" id="name" />
-            <input type="email" name="email" placeholder="E-mail" id="mail" />
-            <textarea name="message" rows="7" placeholder='Message' required></textarea>
-            <button>Send message</button>
+          <form onSubmit={submitHandler}>
+            <input
+              required
+              value={enteredName}
+              onChange={(e) => {
+                setEnteredName(e.target.value);
+              }}
+              type="text"
+              name="name"
+              placeholder="Full Name"
+              id="name"
+            />
+            <input
+              required
+              value={enteredmail}
+              onChange={(e) => {
+                setEnteredMail(e.target.value);
+              }}
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              id="mail"
+            />
+            <textarea
+              value={enteredText}
+              onChange={(e) => {
+                setEnteredText(e.target.value);
+              }}
+              name="message"
+              rows="7"
+              placeholder="Message"
+              required
+            ></textarea>
+            {showStatus && (
+              <span>
+                {"Message sent Successfully"}
+              </span>
+            )}
+            <button type="submit">Send message</button>
           </form>
         </div>
         <div className={classes.right}>
